dbb5606fe6853362a9b24b5aa1ae9009821d8ac9
rana <trana51997@gmail.com>
2023-09-21 18:57:50 -0500

    Login functionality done

--- models/userModel.js
+++ models/userModel.js
@@ -22,6 +22,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide a password"],
     minLength: [8, "Password should have 8 or more characters"],
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -47,6 +48,13 @@ userSchema.pre("save", async function (next) {
 
   next();
 });
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model("User", userSchema);
 
