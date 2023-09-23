0913d9c36931f8f16cacfb15ca6aadbd4bf1316b
rana <trana51997@gmail.com>
2023-09-23 11:28:22 -0500

    Setting security http headers and limiting requests from same api done

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -8,11 +10,25 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
 
+// Development logging
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
-app.use(express.json());
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // app.use((req, res, next) => {
