100060dc03fc3a13752bdee1e77a2b20992aefc2
rana <trana51997@gmail.com>
2023-09-23 11:53:55 -0500

    Preventing parameter pollution done

--- app.js
+++ app.js
@@ -37,8 +37,19 @@ app.use(mongoSanitize());
 // Data sanitization against XSS
 app.use(xss());
 
-// Prevent parameter pollution
-app.use(hpp());
+// Preventing parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
