21fcda397dbc4124907b7220858d9d03859bf1ef
rana <trana51997@gmail.com>
2023-09-23 10:11:33 -0500

    Deleting or deactivating logged in user done

--- models/userModel.js
+++ models/userModel.js
@@ -44,6 +44,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetTokenExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 
 userSchema.pre("save", async function (next) {
@@ -62,6 +67,13 @@ userSchema.pre("save", function (next) {
   if (!this.isModified("password") || this.isNew) return next();
 
   this.passwordChangedAt = Date.now() - 1000;
+
+  next();
+});
+
+userSchema.pre(/^find/, function (next) {
+  // this points to current document
+  this.find({ active: { $ne: false } });
 
   next();
 });
