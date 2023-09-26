36a32d31345c3666504ce1a5c98f1f026cbbd5aa
rana <trana51997@gmail.com>
2023-09-26 14:41:53 -0500

    Preventing duplicate reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -9,8 +9,9 @@ const reviewSchema = new mongoose.Schema(
     },
     rating: {
       type: Number,
-      min: 1,
-      max: 5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
+      set: (val) => Math.round(val * 10) / 10,
     },
     createdAt: {
       type: Date,
@@ -33,6 +34,8 @@ const reviewSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   }
 );
+
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
 
 reviewSchema.pre(/^find/, function (next) {
   // this.populate({
